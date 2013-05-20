cdba96f3ddb586bb3e08a5cb699ce7960e3bb873
Matt Cantillon <matthew.cantillon@gmail.com>
2013-05-20 12:33:41 +0100

    Added missing logic gate methods to Boolean Extension

--- src/NBasicExtensionMethod.Test.Fast/BooleanExtensionTests.cs
+++ src/NBasicExtensionMethod.Test.Fast/BooleanExtensionTests.cs
@@ -7,49 +7,79 @@ namespace NBasicExtensionMethod.Test.Fast
     [TestFixture]
     public class BooleanExtensionTests
     {
-        [Test]
-        public void And_Should_Return_True_When_Both_Values_Are_True()
-        {
-            // Arrange
-            bool bValueA = true;
-            bool bValueB = true;
-
-            // Act & Assert
-            bValueA.And(bValueB).Should().Be(true);
-        }
 
         [Test]
-        [TestCase(true, false)]
-        [TestCase(false, true)]
-        public void And_Should_Return_False_When_One_Value_Is_False(bool bValueA, bool bValueB)
+        [TestCase(true, true, true)]
+        [TestCase(true, false, false)]
+        [TestCase(false, true, false)]
+        [TestCase(false, false, false)]
+        public void And_Should_Return_False_When_One_Value_Is_False(bool bValueA, bool bValueB, bool bExpectedResult)
         {
             // Arrange
           
             // Act & Assert
-            bValueA.And(bValueB).Should().Be(false);
+            bValueA.And(bValueB).Should().Be(bExpectedResult);
         }
 
         [Test]
-        public void Or_Should_Return_False_When_Both_Values_Are_False()
-        {
-            // Arrange
-            bool bValueA = false;
-            bool bValueB = false;
-
-            // Act & Assert
-            bValueA.Or(bValueB).Should().Be(false);
-        }
-
-        [Test]
-        [TestCase(false, true)]
-        [TestCase(true, false)]
-        [TestCase(true, true)]
-        public void Or_Should_Return_True_When_One_Value_Is_True(bool bValueA, bool bValueB)
+        [TestCase(false, true, true)]
+        [TestCase(true, false, true)]
+        [TestCase(true, true, true)]
+        [TestCase(false, false, false)]
+        public void Or_Should_Return_True_When_One_Value_Is_True(bool bValueA, bool bValueB, bool bExpectedResult)
         {
             // Arrange
 
             // Act & Assert
-            bValueA.Or(bValueB).Should().Be(true);
+            bValueA.Or(bValueB).Should().Be(bExpectedResult);
+        }
+
+        [Test]
+        [TestCase(true, false)]    
+        [TestCase(false, true)]    
+        public void Not_ReturnsOppositeOfPassedValue(bool bValueA, bool bExpectedResult)
+        {
+            bValueA.Not().Should().Be(bExpectedResult);
+        }
+
+        [Test]
+        [TestCase(true,true,false)]
+        [TestCase(false,false,true)]
+        [TestCase(true,false,true)]
+        [TestCase(false,true,true)]
+        public void Nand_ReturnsExpectedValueForPassedParameters(bool bValueA, bool bValueB, bool bExpectedResult)
+        {
+            bValueA.NAnd(bValueB).Should().Be(bExpectedResult);
+        }
+
+        [Test]
+        [TestCase(true, true, true)]
+        [TestCase(false, false, true)]
+        [TestCase(true, false, false)]
+        [TestCase(false, true, false)]
+        public void XNOr_WhenValuesAreTheSame_ReturnsTrue(bool bValueA, bool bValueB, bool bExpectedResult)
+        {
+            bValueA.XNOr(bValueB).Should().Be(bExpectedResult);
+        }
+
+        [Test]
+        [TestCase(true, true, false)]
+        [TestCase(false, false, true)]
+        [TestCase(true, false, false)]
+        [TestCase(false, true, false)]
+        public void NOr_WhenBothValuesAreFalse_ReturnsTrue(bool bValueA, bool bValueB, bool bExpectedResult)
+        {
+            bValueA.NOr(bValueB).Should().Be(bExpectedResult);
+        }
+
+        [Test]
+        [TestCase(true, true, false)]
+        [TestCase(false, false, false)]
+        [TestCase(true, false, true)]
+        [TestCase(false, true, true)]
+        public void XOr_WhenValuesAreDifferent_ReturnsTrue(bool bValueA, bool bValueB, bool bExpectedResult)
+        {
+            bValueA.XOr(bValueB).Should().Be(bExpectedResult);
         }
     }
 }
--- src/NBasicExtensionMethod/BooleanExtensions.cs
+++ src/NBasicExtensionMethod/BooleanExtensions.cs
@@ -11,5 +11,31 @@
         {
             return b1 || b2;
         }
+
+        public static bool Not(this bool b1)
+        {
+            return !b1;
+        }
+
+        public static bool NAnd(this bool b1, bool b2)
+        {
+            return !(b1 && b2);
+        }
+
+        public static bool XNOr(this bool b1, bool b2)
+        {
+            return !(b1 != b2);
+        }
+
+        public static bool NOr(this bool b1, bool b2)
+        {
+            return !(b1 || b2);
+        }
+
+        public static bool XOr(this bool b1, bool b2)
+        {
+            return (b1 != b2);
+        }
+
     }
 }
